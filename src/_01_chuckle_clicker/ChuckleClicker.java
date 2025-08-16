19d84b0155ba0a717fc7559cee011bb7eb644f20
league <maddoxseul@sbsd.net>
2025-08-16 11:29:26 -0700

    chuckleclicker, jeopardy

--- src/_01_chuckle_clicker/ChuckleClicker.java
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -2,45 +2,50 @@ package _01_chuckle_clicker;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
+public class ChuckleClicker implements ActionListener {
 	JButton button = new JButton("Joke");
-	JButton button2 = new JButton("Punchline"); 
-	
-	
+	JButton button2 = new JButton("Punchline");
+
 	public void makeButtons() {
-		JFrame frame = new JFrame(); 
+		JFrame frame = new JFrame();
+		frame.setTitle("ChuckleClicker");
 		JPanel panel = new JPanel();
-		frame.setLayout(new FlowLayout());
-		 
-		button.addActionListener(null);
-		button2.addActionListener(new ActionListener());
-		
-		frame.setVisible(true);
+
+		button.addActionListener(this);
+		button2.addActionListener(this);
+
 		frame.add(panel);
-		frame.add(button);
-		frame.add(button2);
-		
-	
-		//if(event.getSource() == button) {
-			
-		//}
-		frame.pack();  
-		
-	
-		
+		panel.add(button);
+		panel.add(button2);
+
+		frame.pack();
+		frame.setVisible(true);
+
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		ChuckleClicker click = new ChuckleClicker();
 		click.makeButtons();
-	
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		if (event.getSource() == button) {
+			JOptionPane.showMessageDialog(null, "What tree can fit in your hand?");
+		}
+		if (event.getSource() == button2) {
+			JOptionPane.showMessageDialog(null, "A palm tree");
+		}
+
 	}
 
 }
