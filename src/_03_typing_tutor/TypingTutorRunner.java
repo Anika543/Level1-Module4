5cd1d6f5124d2e0dad8dde07671bcd428bc615d7
league <maddoxseul@sbsd.net>
2025-08-23 11:30:18 -0700

    Typing Tutor, Book of Illusions

---
+++ src/_03_typing_tutor/TypingTutorRunner.java
@@ -0,0 +1,14 @@
+package _03_typing_tutor;
+
+import java.util.Random;
+
+public class TypingTutorRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TypingTutor x = new TypingTutor();
+		x.setup(); 
+	}
+	
+
+}
--- src/_04_book_of_illusions/BookOfIllusions.java
+++ src/_04_book_of_illusions/BookOfIllusions.java
@@ -6,11 +6,13 @@ package _04_book_of_illusions;
  */
 
 import java.awt.event.MouseAdapter;
+
 import java.awt.event.MouseEvent;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
@@ -18,27 +20,45 @@ import javax.swing.JLabel;
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
+	JFrame frame = new JFrame(); 
+	JLabel label;
 
 	public void run() {
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
+		frame.setSize(500, 600);
 		// 4. find 2 images and save them to your project’s _06_book_of_illusions folder
+		
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String s = "3.jpeg"; 
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		label = loadImageFromComputer(s); 
 		// 8. add your JLabel to the frame
+		frame.add(label); 
 		// 9. call the pack() method on the frame
+		frame.pack(); 
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
-		// 12. remove everything from the frame that was added earlier
+		System.out.println("clicked"); 
+		// 12. remove everything from the frame that was added earlier 
+		frame.remove(label);
 		// 13. load a new image like before (this is more than one line of code)
+		String b = "illusion-2.jpeg"; 
+		JLabel label2 = loadImageFromComputer(b); 
+		frame.add(label2); 
 		// 14. pack the frame
+		frame.pack(); 
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
+	//JOptionPane.showMessageDialog(null, "hi")
 
 	/*
 	 * To use this method, the image must be placed in your Eclipse project under "default package".
